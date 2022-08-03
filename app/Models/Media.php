51ac2094f38a693a4db62c8dec47b1ac7518cda5
Imam Setiawan <s.imam29@gmail.com>
2022-08-03 08:53:37 +0700

    update medias path accessor

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use lluminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use App\Models\Product;
 
 class Media extends Model
@@ -12,6 +13,13 @@ class Media extends Model
     use HasFactory;
 
     protected $table = 'medias';
+
+    protected function path(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => env('APP_URL').'/storage'.$value,
+        );
+    }
 
     public function product(): BelongsTo
     {
