56b6e077e199e00333141338def3708f1ccac612
Imam Setiawan <s.imam29@gmail.com>
2022-08-07 22:10:05 +0700

    implement queue on sending email

--- app/Mail/PaymentReceivedMail.php
+++ app/Mail/PaymentReceivedMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PaymentReceivedMail extends Mailable
+class PaymentReceivedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
